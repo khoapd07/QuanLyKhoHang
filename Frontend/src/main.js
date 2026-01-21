f715cc9ac98540e29dd99397bc32235d2fc52665
khoapd07 <khoapd07@gmail.com>
2026-01-21 13:25:55 +0700

    cap nhat them bootstrap

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// import router from './router' // <-- Import router
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 createApp(App).mount('#app')
